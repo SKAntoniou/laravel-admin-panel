651c4c9d8f330d043f26c53c3f8c29e43de84537
SKAntoniou <SKAntoniou@gmail.com>
2025-05-19 09:28:17 +0100

    factories done

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,11 @@ class EmployeeFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Company::factory(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone_number' => fake()->mobileNumber()
         ];
     }
 }
